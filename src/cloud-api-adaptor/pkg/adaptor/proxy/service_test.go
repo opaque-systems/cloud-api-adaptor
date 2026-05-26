efb7b65b54a2d8f53d0f38d42eeba22aeb9f6ec2
Hernan Gatta <hernan.gatta@opaque.co>
2026-05-26 20:41:37 +0000

    adaptor/proxy: swap digests, if available

--- src/cloud-api-adaptor/pkg/adaptor/proxy/service_test.go
+++ src/cloud-api-adaptor/pkg/adaptor/proxy/service_test.go
@@ -71,3 +71,29 @@ func prepareVolumeDir(directVolumesDir, volumePath string) error {
 
 	return nil
 }
+
+func TestSwap(t *testing.T) {
+	var inout = []struct {
+		in  string
+		out string
+	}{
+		{"something", "something@sha256:abc123"},
+		{"something:latest", "something@sha256:abc123"},
+		{"something@sha256:abc123", "something@sha256:abc123"},
+		{"something@sha256:abc456", "something@sha256:abc123"},
+
+		{"docker.io/library/something", "docker.io/library/something@sha256:abc123"},
+		{"docker.io/library/something:latest", "docker.io/library/something@sha256:abc123"},
+		{"docker.io/library/something@sha256:abc123", "docker.io/library/something@sha256:abc123"},
+		{"docker.io/library/something@sha256:abc456", "docker.io/library/something@sha256:abc123"},
+
+		{"docker.io:8888/library/something", "docker.io:8888/library/something@sha256:abc123"},
+		{"docker.io:8888/library/something:latest", "docker.io:8888/library/something@sha256:abc123"},
+		{"docker.io:8888/library/something@sha256:abc123", "docker.io:8888/library/something@sha256:abc123"},
+		{"docker.io:8888/library/something@sha256:abc456", "docker.io:8888/library/something@sha256:abc123"},
+	}
+
+	for _, tt := range inout {
+		assert.Equal(t, tt.out, swapTagForDigest(tt.in, "sha256:abc123"))
+	}
+}
