7ab1f24d6c94f7df46d399a08182a294d6e99741
Qiang Xu <qiang@opaque.co>
2026-06-23 21:30:09 +0000

    Run extend-rtmr3.sh only once

--- src/cloud-api-adaptor/podvm/files/usr/local/bin/extend-rtmr3.sh
+++ src/cloud-api-adaptor/podvm/files/usr/local/bin/extend-rtmr3.sh
@@ -3,8 +3,17 @@
 set -euxo pipefail
 
 RTMR3="/sys/devices/virtual/misc/tdx_guest/measurements/rtmr3:sha384"
+DIGEST_FILE="/var/run/peerpod/initdata.digest"
+INIT_VAL=$(printf "%096d" 0)
 
 if [ -f $RTMR3 ]; then
-    echo "Extend RTMR3 ..."
-    awk '{printf "%s%032d", $0, 0}' /var/run/peerpod/initdata.digest | xxd -r -p > $RTMR3
+    CURRUENT_VAL=$(cat $RTMR3 | xxd -p -c 0)
+    if [[ "$CURRUENT_VAL" == "$INIT_VAL" ]]; then
+        echo "Extending RTMR3 ..."
+        awk '{printf "%s%032d", $0, 0}' $DIGEST_FILE | xxd -r -p > $RTMR3
+    else
+        echo "RTMR3 has been extended, skip extending"
+    fi
+else
+    echo "Non-TDX TEE is not supported yet."
 fi
