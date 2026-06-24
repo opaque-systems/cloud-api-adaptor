ee7dfddc2b36a8283b3bacbe1b8efe7eaa3e8657
Matei Dibu <contact@mateidibu.dev>
2026-06-24 11:19:18 +0300

    fix: cleanup extend-rtmr3.sh

--- src/cloud-api-adaptor/podvm/files/usr/local/bin/extend-rtmr3.sh
+++ src/cloud-api-adaptor/podvm/files/usr/local/bin/extend-rtmr3.sh
@@ -1,19 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euxo pipefail
 
-RTMR3="/sys/devices/virtual/misc/tdx_guest/measurements/rtmr3:sha384"
-DIGEST_FILE="/var/run/peerpod/initdata.digest"
+RTMR3='/sys/devices/virtual/misc/tdx_guest/measurements/rtmr3:sha384'
+
+if [[ ! -f ${RTMR3} ]]; then
+	echo "${RTMR3} does not exist"
+	echo 'Non-TDX TEE is not supported yet'
+	exit 0
+fi
+
+CURRENT_VAL=$(xxd -p -c 0 "${RTMR3}")
 INIT_VAL=$(printf "%096d" 0)
 
-if [ -f $RTMR3 ]; then
-    CURRUENT_VAL=$(cat $RTMR3 | xxd -p -c 0)
-    if [[ "$CURRUENT_VAL" == "$INIT_VAL" ]]; then
-        echo "Extending RTMR3 ..."
-        awk '{printf "%s%032d", $0, 0}' $DIGEST_FILE | xxd -r -p > $RTMR3
-    else
-        echo "RTMR3 has been extended, skip extending"
-    fi
-else
-    echo "Non-TDX TEE is not supported yet."
+if [[ ${CURRENT_VAL} != "${INIT_VAL}" ]]; then
+	echo "RTMR3 has been extended already, skip extending; current value: ${CURRENT_VAL}"
+	exit 0
 fi
+
+DIGEST_FILE='/var/run/peerpod/initdata.digest'
+
+echo 'Extending RTMR3 ...'
+awk '{printf "%s%032d", $0, 0}' "${DIGEST_FILE}" | xxd -r -p -c 0 >"${RTMR3}"
+
+NEW_VAL=$(xxd -p -c 0 "${RTMR3}")
+echo "RTMR3 extension successful; new value: ${NEW_VAL}"
